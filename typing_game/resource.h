6427fcfa1eb8cbb7a3c0bc3072c35a515d690760
jfennimo <jfennimo@unca.edu>
2025-05-17 18:24:56 -0400

    Updated icon

--- typing_game/resource.h
+++ typing_game/resource.h
@@ -2,7 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by typing_game.rc
 //
-#define IDI_ICON1 ICON "Tombstone.ico"
+#define IDI_ICON1                       101
 
 // Next default values for new objects
 // 
